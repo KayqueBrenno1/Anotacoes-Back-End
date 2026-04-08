cd8d8779d7653eeb01eaf34bdd9f67b8e4741e6f
KAYQUE BRENNO FERREIRA ALMEIDA <25203698@CFP127ALUNO.LOCAL>
2026-04-08 16:37:31 -0300

    feat: finalizar segunda função da missão 05

--- missao 05/modulo/funcoes.js
+++ missao 05/modulo/funcoes.js
@@ -12,3 +12,24 @@ const users = infContatos.contatos['whats-users']
 const getListaTodosUsuario = function () {
     return { users }
 }
+
+const getDadosUsuario = function (number) {
+    let numberTel = String(number)
+    let dadosUsuario = false
+
+    for (let infUser of users) {
+        if (infUser.number == numberTel) {
+            dadosUsuario = {
+                "number": infUser.number,
+                "nome": infUser.account,
+                "nick": infUser.nickname,
+                "account_creation_date": infUser['created-since'].start,
+                "account_closing_date": infUser['created-since'].end,
+                "profile_image": infUser['profile-image'],
+                "background": infUser.background
+            }
+        }
+    }
+
+    return dadosUsuario
+}
